2d24283ae3e31c2bf59adeb92d1de03be3f204dd
JakubRoss <Jakub.Rosploch@gmail.com>
2023-10-13 18:01:38 +0200

    Main entities

---
+++ CarWorkshop.Domain/Entities/CarWorkshop.cs
@@ -0,0 +1,18 @@
+﻿namespace CarWorkshop.Domain.Entities
+{
+    public class CarWorkshop
+    {
+        public required Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime Createdat { get; set; } = DateTime.UtcNow;
+        public CarWorkshopContactDetails ContactDetails { get; set; } = default!;
+
+        public string EncodedName { get; private set; } = default!;
+
+        public void EncodeName()
+        {
+            EncodedName = Name.ToLower().Replace(" ", "-");
+        }
+    }
+}
