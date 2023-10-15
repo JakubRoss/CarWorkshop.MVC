70ad9b37ca454a24eb9f5fc0265634ccb4a761ae
JakubRoss <Jakub.Rosploch@gmail.com>
2023-10-15 09:17:49 +0200

    First migration

---
+++ CarWorkshop.Infrastructure/Migrations/20231015071555_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarWorkshop.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "carWorkshops",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Createdat = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ContactDetails_PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactDetails_Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactDetails_City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactDetails_PostalCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EncodedName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_carWorkshops", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "carWorkshops");
+        }
+    }
+}
