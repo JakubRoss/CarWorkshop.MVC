24d89626029e4998a610539bb1530806402bd284
JakubRoss <Jakub.Rosploch@gmail.com>
2023-11-19 22:18:26 +0100

    model validation

--- CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
+++ CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
@@ -1,11 +1,15 @@
-﻿using CarWorkshop.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CarWorkshop.Application.CarWorkshop
 {
     public class CarWorkshopDto
     {
+        [Required]
+        [StringLength(20,MinimumLength =4)]
         public string Name { get; set; } = default!;
+        [Required]
         public string? Description { get; set; }
+        [Phone]
         public string? PhoneNumber { get; set; }
         public string? Street { get; set; }
         public string? City { get; set; }
--- CarWorkshop.Presentation/Controllers/CarWorkshopController.cs
+++ CarWorkshop.Presentation/Controllers/CarWorkshopController.cs
@@ -13,7 +13,7 @@ namespace CarWorkshop.Presentation.Controllers
             _carworkshopService = carworkshopService;
         }
 
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -21,6 +21,10 @@ namespace CarWorkshop.Presentation.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CarWorkshopDto carWorkshop)
         {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
             await _carworkshopService.Create(carWorkshop);
             return RedirectToAction(nameof(Create));
         }
